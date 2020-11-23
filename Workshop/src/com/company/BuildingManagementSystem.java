5c5a672745b090ace13dc5bb90fe3583dc9e14b6
OliverDahlWagner <71690615+OliverDahlWagner@users.noreply.github.com>
2020-11-23 08:48:12 +0100

    Mangement

--- Workshop/src/com/company/BuildingManagementSystem.java
+++ Workshop/src/com/company/BuildingManagementSystem.java
@@ -1,4 +1,26 @@
 package com.company;
 
+import java.util.List;
+import java.util.UUID;
+
 public class BuildingManagementSystem {
+
+    List<Building> buildings;
+
+    public List<Building> getBuildings() {
+
+        return buildings;
+    }
+
+    public UUID addBuilding(String name){
+        if (!buildings.contains(name)){
+            buildings.add(name);
+        }
+    }
+
+    public void removeBuilding(UUID id){
+        buildings.remove(id);
+    }
+
+
 }
