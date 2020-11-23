c4f4a863931102e1755ae2d881305ebfd08d45c2
Mette-Hansen <mettemariesh@gmail.com>
2020-11-23 08:49:02 +0100

    Added Actuator

--- Workshop/src/com/company/Actuator.java
+++ Workshop/src/com/company/Actuator.java
@@ -1,4 +1,23 @@
 package com.company;
 
-public class Actuator {
+public abstract class Actuator extends Unit {
+    //Attribute
+    double value;
+
+    //Constructor
+    public Actuator(double value){
+        this.value=value;
+    }
+
+    //Methods
+
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
 }
+
--- Workshop/src/com/company/VentilationActuator.java
+++ Workshop/src/com/company/VentilationActuator.java
@@ -1,4 +1,22 @@
 package com.company;
 
-public class VentilationActuator {
+public class VentilationActuator extends Actuator{
+    //Attribute to String
+    String name;
+
+    //Constructor matching super
+    public VentilationActuator(String name, double value) {
+        super(value);
+        this.name = name;
+    }
+
+    @Override
+    public double getValue(){
+        return value;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
 }
