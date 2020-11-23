a50b0c36967523df019e858e3c95a4c8c1ab088f
yunggunder <71690320+yunggunder@users.noreply.github.com>
2020-11-23 10:29:15 +0100

    main

--- Workshop/src/com/company/Main.java
+++ Workshop/src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.util.LinkedList;
+import java.util.UUID;
+
 public class    Main {
 
     public static void main(String[] args) {
-
+    Building building = new Building(new LinkedList<Sensor>(), new LinkedList<Actuator>(), "Kantine", UUID.randomUUID());
+        System.out.println(building.toString());
     }
 }
