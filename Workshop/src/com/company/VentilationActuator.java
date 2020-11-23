7ffe2d7dc8060d8299582b7fb0b84cedbcf58eb3
Mette-Hansen <mettemariesh@gmail.com>
2020-11-23 09:04:30 +0100

    Added changes

--- Workshop/src/com/company/VentilationActuator.java
+++ Workshop/src/com/company/VentilationActuator.java
@@ -1,22 +1,24 @@
 package com.company;
 
-public class VentilationActuator extends Actuator{
+import java.util.UUID;
+
+public class VentilationActuator extends Actuator {
     //Attribute to String
     private String name;
 
     //Constructor matching super
-    public VentilationActuator(String name, double value) {
-        super(value);
+    public VentilationActuator(String name, UUID id, double value) {
+        super(name, id, value);
         this.name = name;
     }
 
     @Override
-    public double getValue(){
+    public double getValue() {
         return value;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return name;
     }
 }
