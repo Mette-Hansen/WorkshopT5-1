4141f9b25f4451f76963cfa1f108f631963aa901
yunggunder <71690320+yunggunder@users.noreply.github.com>
2020-11-23 09:47:24 +0100

    add changes

--- Workshop/src/com/company/Building.java
+++ Workshop/src/com/company/Building.java
@@ -1,2 +1,34 @@
 package com.company;
 
+<<<<<<< Updated upstream
+=======
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+
+public class Building extends Unit {
+
+    private LinkedList<Integer> actuator = new LinkedList<Integer>();
+    private LinkedList<Integer> sensor = new LinkedList<Integer>();
+
+
+    public Building(List sensor, List actuator) {
+
+    }
+
+    public LinkedList getSensors() {
+        return sensor;
+    }
+
+    public LinkedList getActuator() {
+        return actuator;
+    }
+
+    public UUID addTemperatureSensor(String name) {
+        sensor.add(value);
+        return
+    }
+
+}
+>>>>>>> Stashed changes
